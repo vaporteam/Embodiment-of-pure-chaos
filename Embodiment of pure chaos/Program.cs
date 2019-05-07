2efa53c2897464ad3d02b92842ea2d09aaa873eb
Pablo Cruz Mendez <pablo.cruz@metrica.es>
2019-05-07 14:41:10 +0200

    jiofejfoijefmkwkjerfiwojirtui99999999999

---
+++ Embodiment of pure chaos/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embodiment_of_pure_chaos
+{
+
+    class NotOkBro : OkBro
+    {
+        public class H____a : pepe
+        {
+            public static string[] KKKKKKKKKKKKKK { get; } = { "CHE", "zerrrar" };
+
+        }
+    }
+    class OkBro : pepe
+    {
+        public class H____a : pepe
+        {
+            public static string[] EEEEEEEEEEEEEEEEEEEEEEEEE { get; } = { "CHE", "zerrrar" };
+
+        }
+
+        public class mimiimimimimimiimimimimimiimimimimimi                  
+            : Object
+        {
+            public
+         static
+       void
+      RUSHB()
+    {
+  Console.WriteLine("WHAt u kieres faire?");
+    }
+        }
+    }
+    class reflexiones_apensar : pepe
+    {
+                                                            public
+            static       OkBro 
+            QuickStarto(            String      strasdfw)
+        {
+            MethodInfo          fmklwjrwmfkwkeokjrwwfwrfw2343242354354835984749324 =
+                typeof       (lkfiofhiusd).            
+                GetMethod(strasdfw);
+            fmklwjrwmfkwkeokjrwwfwrfw2343242354354835984749324.          Invoke(       null, null);
+            return          null;
+        }
+    }
+    class pepe : Object
+    { }
+        class lkfiofhiusd : pepe
+    {
+        static void Main(string[] args)
+        {
+            for (int LMAO = 0; LMAO != 100000; LMAO++);
+
+            reflexiones_apensar.QuickStarto(NotOkBro.H____a.KKKKKKKKKKKKKK[0]);
+            OkBro.mimiimimimimimiimimimimimiimimimimimi.RUSHB();
+
+
+            reflexiones_apensar.QuickStarto(OkBro.H____a.EEEEEEEEEEEEEEEEEEEEEEEEE[1]);
+        }
+
+        public static int CHE()
+        {
+            Console.Write("H");
+            return CHA();
+        }
+
+        private static int CHA()
+        {
+            Console.Write("I");
+            return CHU();
+        }
+
+        public static int CHU()
+        {
+            Console.WriteLine("!");
+            return 0;
+        }
+
+        public static void zerrrar()
+        {
+            Action Open = () =>
+            {
+                asdfg.oubrir();
+            };
+            Object any = Open.Clone();
+            ((Action)any).Invoke();
+        }
+
+        private static class asdfg
+        {
+           public static void oubrir()
+            {
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
